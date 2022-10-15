93a2a57f7d66ac1f4d73c9b814e7847deb2b4b32
LuisDev <luisgmsn2001@gmail.com>
2022-10-15 11:31:30 -0500

    Mostrar productos en el API

---
+++ Dev.Api/Controllers/Producto/ProductoController.cs
@@ -0,0 +1,25 @@
+﻿using Dev.Business.Procucto;
+using Dev.Model.Response;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dev.Api.Controllers.Producto
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        ProductoBusiness producto;
+
+        public ProductoController()
+        {
+            producto = new ProductoBusiness();
+        }
+        [HttpGet]
+        public ResponseModel<object> getProductos()
+        {
+            return producto.getProductos();
+        }
+
+    }
+}
---
+++ Dev.Business/Procucto/ProductoBusiness.cs
@@ -0,0 +1,35 @@
+﻿using Dev.Data.Repository;
+using Dev.Model.Response;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dev.Business.Procucto
+{
+    public class ProductoBusiness
+    {
+        GeneralRepository<object> repository;
+        public ProductoBusiness()
+        {
+            repository = new GeneralRepository<object>();   
+        }
+
+        public ResponseModel<object> getProductos()
+        {
+            ResponseModel<object> response = new ResponseModel<object>();
+
+            try
+            {
+                response.Result = repository.GetList("sps_productos");
+                response.Code = Enum.StatusCodeEnum.Ok;
+                response.Message = "";
+
+            }catch (Exception e)
+            {
+                response.Message= $"Algo salió mal Error => {e.Message}";
+                response.Code = Enum.StatusCodeEnum.Bad_Request;
+            }
+            return response;
+        }
+    }
+}
