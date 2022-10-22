efc64a50db93555de9170054aefe45a69032b747
luisgmsn2001@gmail.com <>
2022-10-22 11:09:42 -0500

    Actualización del 22/10/2022, creación de procedimientos que permiten actualizar, insertar, mostrar productos  del stock

--- Dev.Api/Controllers/Producto/ProductoController.cs
+++ Dev.Api/Controllers/Producto/ProductoController.cs
@@ -20,6 +20,27 @@ namespace Dev.Api.Controllers.Producto
         {
             return producto.getProductos();
         }
+        [HttpGet]
+        public ResponseModel<object> getProductoById(int id)
+        {
+            return producto.getProductoById(id);
+        }
+        #region Post
+        [HttpPost]
+        public ResponseModel<int> InsertaProducto(string nombre, double precio, int cantidad)
+        {
+            return producto.InsertProducto(nombre, precio, cantidad);
+        }
+        #endregion
+
+        #region Post
+        [HttpPost]
+        public ResponseModel<int> UpdateProducto(int id, string nombre, double precio, int cantidad)
+        {
+            return producto.UpdateProducto(id, nombre, precio, cantidad);
+        }
+        #endregion
+
 
     }
 }
--- Dev.Business/Procucto/ProductoBusiness.cs
+++ Dev.Business/Procucto/ProductoBusiness.cs
@@ -1,7 +1,9 @@
-﻿using Dev.Data.Repository;
+﻿using Dapper;
+using Dev.Data.Repository;
 using Dev.Model.Response;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace Dev.Business.Procucto
@@ -24,12 +26,84 @@ namespace Dev.Business.Procucto
                 response.Code = Enum.StatusCodeEnum.Ok;
                 response.Message = "";
 
-            }catch (Exception e)
+            }
+            catch (Exception e)
             {
-                response.Message= $"Algo salió mal Error => {e.Message}";
+                response.Message = $"Algo salió mal Error => {e.Message}";
                 response.Code = Enum.StatusCodeEnum.Bad_Request;
             }
             return response;
         }
+
+        public ResponseModel<object> getProductoById(int id)
+        {
+            ResponseModel<object> response = new ResponseModel<object>();
+            try
+            {
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("param", id, DbType.Int32);//renombrar parametro
+
+
+                response.Result = repository.GetSingle("sps_producto", parameters);//renombrar store
+                response.Code = Enum.StatusCodeEnum.Ok;
+                response.Message = "";
+            }
+            catch (Exception e)
+            {
+                response.Result = -1;
+                response.Code = Enum.StatusCodeEnum.Bad_Request;
+                response.Message = e.Message;
+            }
+            return response;
+        }
+        public ResponseModel<int> InsertProducto(string nombre, double precio, int cantidad)
+        {
+            ResponseModel<int> response = new ResponseModel<int>();
+            try
+            {
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("p_nombre", nombre, DbType.String);
+                parameters.Add("p_precio", precio, DbType.Double);
+                parameters.Add("p_stock", cantidad, DbType.Int32);
+
+
+                response.Result = repository.Execute("spi_producto", parameters, true);
+                response.Code = Enum.StatusCodeEnum.Ok;
+                response.Message = "";
+            }
+            catch (Exception e)
+            {
+                response.Result = -1;
+                response.Code = Enum.StatusCodeEnum.Bad_Request;
+                response.Message = e.Message;
+            }
+            return response;
+        }
+
+        public ResponseModel<int> UpdateProducto(int id,string nombre, double precio, int cantidad)
+        {
+            ResponseModel<int> response = new ResponseModel<int>();
+            try
+            {
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("product", id, DbType.Int32);
+                parameters.Add("pname", nombre, DbType.String);
+                parameters.Add("price", precio, DbType.Double);
+                parameters.Add("stock", cantidad, DbType.Int32);
+
+
+                response.Result = repository.Execute("spu_productos", parameters, true);
+                response.Code = Enum.StatusCodeEnum.Ok;
+                response.Message = "";
+            }
+            catch (Exception e)
+            {
+                response.Result = -1;
+                response.Code = Enum.StatusCodeEnum.Bad_Request;
+                response.Message = e.Message;
+            }
+            return response;
+        }
+
     }
 }
